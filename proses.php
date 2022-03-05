37b222bc9ddcf494a5492c018e5095800e1dbe93
hudsam <app.hoedhya@gmail.com>
2022-03-05 19:59:17 +0700

    ➕ front page w/ navbar

---
+++ proses.php
@@ -0,0 +1,14 @@
+<?php
+    $title = 'Proses Pemesanan';
+    include 'template/header.php';
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md">
+            <h3 class="lead fw-bold"><i class="bi bi-info-circle text-primary"></i> Hi, Eco-Clean!</h3>
+        </div>
+    </div>
+</div>
+
+<?php include 'template/footer.php'; ?>

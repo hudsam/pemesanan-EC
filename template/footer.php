99a005bf829d1f475e2f95e0ff9c59c7b24d9082
hudsam <app.hoedhya@gmail.com>
2022-03-11 01:09:41 +0700

    ➕ aktivasi tooltips dimanapun

--- template/footer.php
+++ template/footer.php
@@ -1,4 +1,10 @@
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" type="text/javascript" rel="script"></script>
     <script src="assets/vendors/bootstrap-5.1.3/js/bootstrap.bundle.min.js" type="text/javascript" rel="script"></script>
-    <script src="assets/pemesanan.js" type="text/javascript" rel="script"></script>
+    <script type="text/javascript" rel="script">
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+    </script>
 </body>
 </html>
